cfe0b45e773ffdc72cba430030d6c93918b3280c
Shivani Joshi <57892587+Shivani-joshi-21@users.noreply.github.com>
2021-06-02 14:49:14 +0530

    Create Wave Array.py

---
+++ Wave Array.py
@@ -0,0 +1,57 @@
+'''Given a sorted array arr[] of distinct integers. Sort the array into a wave-like array and return it. In other words, arrange the elements into a sequence such that a1 >= a2 <= a3 >= a4 <= a5..... (considering the increasing lexicographical order).
+
+Example 1:
+
+Input:
+n = 5
+arr[] = {1,2,3,4,5}
+Output: 2 1 4 3 5
+Explanation: Array elements after 
+sorting it in wave form are 
+2 1 4 3 5.
+Example 2:
+
+Input:
+n = 6
+arr[] = {2,4,7,8,9,10}
+Output: 4 2 8 7 10 9
+Explanation: Array elements after 
+sorting it in wave form are 
+4 2 8 7 10 9.
+Your Task:
+The task is to complete the function convertToWave() which converts the given array to wave array.'''
+
+
+class Solution:
+    #Complete this function
+    #Function to sort the array into a wave-like array.
+    def convertToWave(self,A,N):
+        #Your code here
+        for i in range(0,len(A)-1,2):
+            A[i],A[i+1]=A[i+1],A[i]
+        return A
+
+import math
+
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        N=int(input())
+        
+        A=[int(x) for x in input().split()]
+        ob=Solution()
+        ob.convertToWave(A,N)
+        for i in A:
+            print(i,end=" ")
+        
+        
+        print()
+        
+       
+        T-=1
+
+
