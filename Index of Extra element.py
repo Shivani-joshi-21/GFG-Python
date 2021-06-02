25b91fb0847c6a8f6d415971715247955db5dd9b
Shivani Joshi <57892587+Shivani-joshi-21@users.noreply.github.com>
2021-06-02 22:20:16 +0530

    Create Index of Extra element.py

---
+++ Index of Extra element.py
@@ -0,0 +1,32 @@
+'''Given two sorted arrays of distinct elements. There is only 1 difference between the arrays. First array has one element extra added in between. Find the index of the extra element.
+
+Example 1:
+
+Input:
+N = 7
+A[] = {2,4,6,8,9,10,12}
+B[] = {2,4,6,8,10,12}
+Output: 4
+Explanation: In the second array, 9 is
+missing and it's index in the first array
+is 4.'''
+
+
+class Solution:
+    
+    def findExtra(self,a,b,n):
+    
+    #add code here
+        diff = list(set(a)-set(b))
+        y = ''.join(map(str , diff))
+        return (a.index(int(y)))
+#{ 
+#  Driver Code Starts
+if __name__ == "__main__":
+    t=int(input())
+    for i in range(t):
+        n=int(input())
+        a=list(map(int,input().split()))
+        b=list(map(int,input().split()))
+        print(Solution().findExtra(a,b,n))
+# } Driver Code Ends
