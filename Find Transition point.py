19b9d814cd22bcd6bdcbfc17d39ca40322be767c
Shivani Joshi <57892587+Shivani-joshi-21@users.noreply.github.com>
2021-06-02 18:47:29 +0530

    Create Find Transition point.py

---
+++ Find Transition point.py
@@ -0,0 +1,31 @@
+'''Given a sorted array containing only 0s and 1s, find the transition point. 
+
+
+Example 1:
+
+Input:
+N = 5
+arr[] = {0,0,0,1,1}
+Output: 3
+Explanation: index 3 is the transition 
+point where 1 begins.'''
+
+def transitionPoint(arr, n):
+    #Code here
+    for i in arr:
+        if 1 in arr:
+            return (arr.index(1))
+        else:
+            return -1
+
+#{ 
+#  Driver Code Starts
+#driver code
+if __name__=='__main__':
+    t=int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        print(transitionPoint(arr, n))
+
+# } Driver Code Ends
